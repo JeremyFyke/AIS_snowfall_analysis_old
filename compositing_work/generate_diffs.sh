9f30f6af5905fcf94fe8ab52e6423e6fe3b78859
Jeremy Fyke <fyke@lanl.gov>
2017-04-28 13:36:42 -0600

    In middle of fixing code for Hailong

--- compositing_work/generate_diffs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for n in {1..27}; do
-   echo $n
-   ncra -O output/high/Basin"$n"_ssticetemp_fv0.9x1.25.nc high.nc
-   ncra -O output/low/Basin"$n"_ssticetemp_fv0.9x1.25.nc low.nc
-   ncdiff -O high.nc low.nc output/diffs/$n.nc
-   rm high.nc low.nc
-done
